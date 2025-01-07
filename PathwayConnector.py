1830003a69875fe8029000c86bca39cec4fe7713
Shuvraneel Mitra <mitrashuvraneel@gmail.com>
2025-01-07 21:59:03 +0530

    Created a simple Pathway Connector to GDrive

--- PathwayConnector.py
+++ PathwayConnector.py
@@ -1,2 +1,12 @@
 import pathway as pw
+from dotenv import load_dotenv
+import os
 
+load_dotenv()
+
+table = pw.io.gdrive.read(
+    object_id=os.environ.get("OBJECT_ID"),
+    service_user_credentials_file="credentials.json"
+)
+print(dir(table))
+pw.run()
