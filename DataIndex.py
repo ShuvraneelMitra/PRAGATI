6a7604f8dbdf93fb782517eaecc67fdb8b079fd8
Shuvraneel Mitra <mitrashuvraneel@gmail.com>
2025-01-09 01:30:16 +0530

    Set up data indexing

---
+++ DataIndex.py
@@ -0,0 +1,9 @@
+import logging 
+import sys
+import time
+
+logging.basicConfig(level=logging.INFO, 
+                    format='%(name)s - %(levelname)s - %(message)s', 
+                    filename='kdsh-2025.log')
+
+
