ee23ddc5ea9a2a04ace1d51c8d057a88022f4a03
Shuvraneel Mitra <mitrashuvraneel@gmail.com>
2025-01-09 06:53:30 +0530

    Implemented DataIndex class

--- PathwayConnector.py
+++ PathwayConnector.py
@@ -5,8 +5,9 @@ import os
 load_dotenv()
 
 table = pw.io.gdrive.read(
-    object_id=os.environ.get("OBJECT_ID"),
+    object_id=os.environ.get("TMLR_OBJECT_ID"),
     service_user_credentials_file="credentials.json"
 )
+
 print(dir(table))
 pw.run()
